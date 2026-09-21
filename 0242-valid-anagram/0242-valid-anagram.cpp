56cb1f425fc03cafe6e2cc884be350893a69f121
HarpalKalankar <harpalkalankar7@gmail.com>
2026-09-21 23:51:29 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        std::sort(s.begin(),s.end());
-        std::sort(t.begin(),t.end());
-        if(s == t){
-            return true;
-        }
-        else{
+        if (s.length() != t.length()){
             return false;
         }
+        unordered_map<char , int > countS;
+        unordered_map<char , int > countT;
+        for (int i = 0 ; i < s.size(); i++){
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+        return countS == countT ;
     }
 };
